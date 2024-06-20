38bd4dd10b882a3ca937fead49df4e6d196a5fe9
Daniele Lolli <uncledan74@gmail.com>
2024-06-20 10:31:11 +0200

    scf1

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="icon" type="image/png" href="/wp-content/uploads/2024/06/Logo_CdL_512x512_70.png" />
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
